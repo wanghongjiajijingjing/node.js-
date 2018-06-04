ededd70c094b2bf93a510f5a26fc1dffef2a30a3
wanghongjiajijingjing <1323565010@qq.com>
2018-06-04 20:09:41 +0800

    my project

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:"mongodb://todo:123456@ds217310.mlab.com:17310/todo",
+  secretOrKey:"secret"
+}
---
+++ routes/api/users.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+// const gravatar = require("gravatar");
+const keys = require("../../config/keys");
+const passport = require("passport");
+const User = require("../../models/User");
+
+router.get("/test",(req,res) => {
+  res.json({msg:"login works"});
+});
+
+// $route POST api/users/register
+router.post("/register",(req,res) => {
+  // 查询数据库中是否拥有邮箱
+  User.findOne({email:req.body.email})
+      .then((user) => {
+        if(user){
+          return res.status(400).json({email:"邮箱已被注册！"});
+        }
+        else{
+          // const avatar = gravatar.url(req.body.email, {s: "200", r: "pg", d:"mm"});
+          const newUser = new User({
+            name:req.body.name,
+            email:req.body.email,
+            // avatar,
+            password:req.body.password
+          });
+
+          bcrypt.genSalt(10, function(err, salt){
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) throw err;
+
+              newUser.password = hash;
+
+              newUser.save()
+                     .then(user => res.json(user))
+                     .catch(err => console.log(err));
+            });
+          });
+        }
+      })
+})
+
+// $route POST api/users/login
+router.post("/login",(req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  // 查询数据库
+  User.findOne({email})
+      .then(user => {
+        if(!user){
+          return res.status(404).json({email:"用户不存在！"});
+        }
+
+        // 密码匹配
+        bcrypt.compare(password, user.password)
+              .then(isMatch => {
+                if(isMatch){
+                  const rule = {id:user.id, name:user.name};
+                  jwt.sign(rule, keys.secretOrKey, {expiresIn:3600}, (err, token) => {
+                    if(err) throw err;
+                    res.json({
+                      success:true,
+                      token:"Bearer"+ token
+                    });
+                  })
+                }
+                else{
+                  return res.status(400).json({password:"密码错误！"});
+                }
+              })
+      })
+})
+
+// $route GET api/users/current
+// @desc return current user
+// @access private
+router.get("/current", passport.authenticate("jwt", {session: false }), (req,res) => {
+  res.json({
+    id:req.user.id,
+    name:req.user.name,
+    email:req.user.email
+  });
+})
+
+module.exports = router;
